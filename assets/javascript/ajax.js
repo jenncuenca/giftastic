e914693c3a4eb0b27f4d750efda61c0f8e7c7d7c
Jennifer Cuenca <>
2018-03-04 21:28:47 -0500

    creation of button using form

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,5 +1,26 @@
-$("button").on("click", function() {
+// Create an array named "topics"
 
+var topics = ['dog', 'cat', 'squirrel', 'dolphin'];
+
+//for loop for going through array to grab data to create buttons
+function buttonCreation () {
+
+    $('#buttons-view').empty();
+
+    for (var i =0; i < topics.length; i++){
+
+    // buttons get created from "topics" array
+    var newBtn = $('<button>').html(topics[i])
+    newBtn.addClass('btn btn-info animal') // adds classes to an jquery html created element
     
+    //button gets appended to html
+    $('#buttons-view').append(newBtn)
+    };
+};
+
+buttonCreation();
+
+
 
 });
+
